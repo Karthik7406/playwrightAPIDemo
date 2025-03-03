5caf03dab6899345e8bb92b361548b763d070389
Karthik7406 <karthikkumar2801@gmail.com>
2025-03-03 22:38:52 +0530

    use API call to update the token instead of the UI login

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,22 +1,50 @@
 import {test as setup } from '@playwright/test';
 import 'dotenv/config'
 
+import user from "../.auth/user.json";
+import fs from "fs";
+
 let userEmail = process.env["EMAIL"]!;
 let password = process.env["PASSWORD"]!;
 
 
 const authFile = ".auth/user.json";
 
-setup("authentication", async({page}) => {
-    await page.goto("https://conduit.bondaracademy.com/");
+setup("authentication", async({request}) => {
 
-  await page.getByText("Sign in").click();
-  await page.getByPlaceholder("Email").fill(userEmail);
-  await page.getByPlaceholder("Password").fill(password);
-  await page.getByRole("button", {name: "Sign in"}).click();
+  // await page.goto("https://conduit.bondaracademy.com/");
 
-  await page.waitForResponse("https://conduit-api.bondaracademy.com/api/tags");
+  // await page.getByText("Sign in").click();
+  // await page.getByPlaceholder("Email").fill(userEmail);
+  // await page.getByPlaceholder("Password").fill(password);
+  // await page.getByRole("button", {name: "Sign in"}).click();
 
-  await page.context().storageState({path: authFile});
+  // await page.waitForResponse("https://conduit-api.bondaracademy.com/api/tags");
+
+  // await page.context().storageState({path: authFile});
+
+  const response = await request.post("https://conduit-api.bondaracademy.com/api/users/login", {
+
+    data: {
+      "user": {
+        "email": userEmail,
+        "password": password
+      }
+    }
+
+  });
+
+  const responseBody = await response.json();
+
+  const accessToken = responseBody?.user?.token;
+  console.log("user data ", user);
+
+  user.origins[0].localStorage[0].value = accessToken
+  fs.writeFileSync(authFile, JSON.stringify(user));
+
+  process.env["ACCESS_TOKEN"] = accessToken;
+
+  console.log("response ", responseBody, process.env.ACCESS_TOKEN);
+
 
 })
